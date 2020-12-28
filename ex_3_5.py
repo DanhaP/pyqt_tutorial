5ed5d9096145bf0a87f4647ac9582dac2b28b6cf
dhk5844 <dhk5844@ajou.ac.kr>
2020-12-28 16:40:43 +0900

    ex 3-5 상태바 만들기

---
+++ ex_3_5.py
@@ -0,0 +1,27 @@
+## Ex 3-5. 상태바 만들기.
+## https://wikidocs.net/21928
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyApp(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        ##self는 MyApp 객체를 지칭.
+        ##QMenuBar, QToolBar, QDockWidget, QStatusBar를 포함.
+        self.statusBar().showMessage('Ready')
+
+        self.setWindowTitle('Statusbar')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
